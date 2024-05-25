c16c3e908da550a5df89f03853ca159f8cca99a6
hakanuyysal <hakanuyysal@gmail.com>
2024-05-25 21:05:20 +0300

    update methode

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,19 @@
 module Api
  class ProductsController < ApplicationController
-    
+    def create
+        @product = Product.create(product_params)
+        @product.save
+        render json: @product 
+    end
+
+    def update 
+        @product = Product.find(params[:id])
+        @product.update(product_params)
+        render json: @product
+    end
+
+    def product_params
+        params.require(:product).permit(:name, :price, :description, :quantity)
+     end 
  end
 end
